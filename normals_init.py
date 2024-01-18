3919bda78481202ca3e4a8cc879190b3c2d78646
RushaliRajesh <rushalirajesh2000@gmail.com>
2024-01-18 16:32:58 +0530

    in

--- normals_init.py
+++ normals_init.py
@@ -7,20 +7,6 @@ from scipy.optimize import minimize
 from scipy.linalg import lstsq
 import os
 
-pcd_list = np.load("xyz_files.npy")
-normals_list = np.load("normals_files.npy")
-
-pcds=[]
-norms = []
-for pcd, normals in zip(pcd_list, normals_list):
-    pcds.append(np.loadtxt(pcd))
-    norms.append(np.loadtxt(normals))
-pcds = np.array(pcds)
-norms = np.array(norms)
-print(pcds.shape, norms.shape)
-
-'''neighborhood (normal init)'''
-init_norms =[]
 def find_neighs(points, k=250):
     nbrs = NearestNeighbors(n_neighbors=k, algorithm= 'kd_tree').fit(points)
     distances, indices = nbrs.kneighbors(points) 
@@ -38,8 +24,23 @@ def process_pcd(pcd):
     norm_esti_pca = Parallel(n_jobs=-1)(delayed(pca_plane)(point_neighs) for point_neighs in neighs)
     return np.array(norm_esti_pca)
     
+if __name__ == "__main__":
 
-for pcd in pcds:
-    init_norms.append(process_pcd(pcd))
-        
-np.save("init_normals.npy", np.array(init_norms))
+    pcd_list = np.load("xyz_files.npy")
+    normals_list = np.load("normals_files.npy")
+
+    pcds=[]
+    norms = []
+    for pcd, normals in zip(pcd_list, normals_list):
+        pcds.append(np.loadtxt(pcd))
+        norms.append(np.loadtxt(normals))
+    pcds = np.array(pcds)
+    norms = np.array(norms)
+    print(pcds.shape, norms.shape)
+
+    '''neighborhood (normal init)'''
+    init_norms =[]
+    for pcd in pcds:
+        init_norms.append(process_pcd(pcd))
+            
+    # np.save("init_normals.npy", np.array(init_norms))
