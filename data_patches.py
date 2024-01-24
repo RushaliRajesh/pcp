3230e9c0cfb4b2535bab0f9b5045941fd3f84f23
RushaliRajesh <rushalirajesh2000@gmail.com>
2024-01-24 18:26:07 +0530

    IN

--- data_patches.py
+++ data_patches.py
@@ -5,31 +5,44 @@ from joblib import Parallel, delayed
 import os
 import torch
 from torch.utils.data import Dataset
-
+from torch_kmeans import KMeans 
 # clean version
 
 '''creating patches via clustering'''
+
+def torch_kmeans(dist_pts, k=10):
+    num_clus = k
+    model = KMeans(n_clusters= num_clus).cuda()
+    labels = model.fit_predict(dist_pts.unsqueeze(0).unsqueeze(2))
+    return labels
 
 def process_one_pcd(main_ind, point_ind=0):
     all_pts = np.load("xyz_points.npy")
     all_norms = np.load("init_normals.npy")
     pts = all_pts[main_ind]
     norms = all_norms[main_ind]
-    print(pts.shape, norms.shape)
+    # print(pts.shape, norms.shape)
     dists_ori, indices_ori = find_neighs(pts)
     neighs = pts[indices_ori]
-    labels_per_row = np.apply_along_axis(lambda row: ckwrap.ckmeans(row, 5).labels, 1, dists_ori)
+    # labels_per_row = np.apply_along_axis(lambda row: ckwrap.ckmeans(row, 5).labels, 1, dists_ori)
+    dists_ori = torch.from_numpy(dists_ori).cuda()
+    dist_pts = dists_ori[point_ind]
+    labels_per_row = torch_kmeans(dist_pts, 10) # shape: (1, 249)
 
-    print(f'using %d gpus: ', torch.cuda.device_count())
+    # print(labels_per_row.device) #this is on cuda:0
+    # return labels_per_row, neighs[0]
+
+    # print(f'using %d gpus: ', torch.cuda.device_count())
     #pts to gpu
     pts = torch.from_numpy(pts).cuda()
     norms = torch.from_numpy(norms).cuda()
     indices_ori = torch.from_numpy(indices_ori).cuda()
-    labels_per_row = torch.from_numpy(labels_per_row).cuda()
+    # labels_per_row = torch.from_numpy(labels_per_row).cuda()
 
     i = point_ind
     ind = indices_ori[i]
-    labels = labels_per_row[i]
+    labels = labels_per_row.squeeze(0)
+    # print(labels.shape)
     # for i,( ind, labels) in enumerate(zip(indices_ori, labels_per_row)):
     center = pts[i]
     mat = []
@@ -62,6 +75,7 @@ def process_one_pcd(main_ind, point_ind=0):
         area = torch.linalg.norm(crs_pdts, dim=1)
         crs_pdts = torch.divide(crs_pdts, torch.linalg.norm(crs_pdts, dim=1, keepdim=True))
         dot_pdts = torch.tensordot(crs_pdts, norms[i].T, dims=1)
+        # print(dot_pdts)
         sign = torch.divide(dot_pdts, torch.abs(dot_pdts))
         area = torch.multiply(area, sign)
         sort_indices = torch.argsort(area)
@@ -74,6 +88,73 @@ def process_one_pcd(main_ind, point_ind=0):
     mat = np.array([tensor.cpu().numpy() for tensor in mat])
 
     return mat
+
+'''the following is the process_one_pcd function with ckmeans that is correct, verified'''
+
+# def process_one_pcd(main_ind, point_ind=0):
+#     all_pts = np.load("xyz_points.npy")
+#     all_norms = np.load("init_normals.npy")
+#     pts = all_pts[main_ind]
+#     norms = all_norms[main_ind]
+#     print(pts.shape, norms.shape)
+#     dists_ori, indices_ori = find_neighs(pts)
+#     neighs = pts[indices_ori]
+#     labels_per_row = np.apply_along_axis(lambda row: ckwrap.ckmeans(row, 5).labels, 1, dists_ori)
+
+#     print(f'using %d gpus: ', torch.cuda.device_count())
+#     #pts to gpu
+#     pts = torch.from_numpy(pts).cuda()
+#     norms = torch.from_numpy(norms).cuda()
+#     indices_ori = torch.from_numpy(indices_ori).cuda()
+#     labels_per_row = torch.from_numpy(labels_per_row).cuda()
+
+#     i = point_ind
+#     ind = indices_ori[i]
+#     labels = labels_per_row[i]
+#     # for i,( ind, labels) in enumerate(zip(indices_ori, labels_per_row)):
+#     center = pts[i]
+#     mat = []
+#     for clus in torch.unique(labels, dim=0):
+#         # ind_clus = ind[np.where(labels == clus)]
+#         ind_clus = ind[torch.where(labels==clus)]
+#         coords = pts[ind_clus]
+
+#         if(coords.shape[0] < 25):
+#             # 2 random integers
+#             for _ in range(25-(coords.shape[0])):
+#                 a, b = torch.randint(1, coords.shape[0], (2,))
+#                 a_n, b_n = torch.divide(a, (a+b)), torch.divide(b, (a+b))
+#                 rnd_pt = torch.add(torch.multiply(a_n, (center-coords[a])), torch.multiply(b_n, (center-coords[b]))).unsqueeze(0)
+#                 coords = torch.cat((coords, rnd_pt), dim=0)
+#         else:
+#             coords = coords[:25]
+        
+#         # print("coords after adding random points: ", coords)
+
+        
+#         vec2 = torch.subtract(center, coords)
+#         closest_ind = torch.argmin(torch.linalg.norm(vec2, dim=1))
+#         closest = coords[closest_ind]
+#         vec1 = torch.subtract(center, closest)
+#         vec2 = torch.cat((vec2[:closest_ind], vec2[closest_ind+1:]), dim=0)
+#         vec1 = torch.divide(vec1, torch.linalg.norm(vec1))
+#         vec2 = torch.divide(vec2, torch.linalg.norm(vec2, dim=1, keepdim=True))
+#         crs_pdts = torch.cross(vec1.expand_as(vec2), vec2)
+#         area = torch.linalg.norm(crs_pdts, dim=1)
+#         crs_pdts = torch.divide(crs_pdts, torch.linalg.norm(crs_pdts, dim=1, keepdim=True))
+#         dot_pdts = torch.tensordot(crs_pdts, norms[i].T, dims=1)
+#         sign = torch.divide(dot_pdts, torch.abs(dot_pdts))
+#         area = torch.multiply(area, sign)
+#         sort_indices = torch.argsort(area)
+#         sorted_coords = coords[sort_indices]
+        
+#         # return dot_pdts, area, crs_pdts, coords, sorted_coords, vec1, vec2, sign, closest, center, closest_ind
+        
+#         mat.append(sorted_coords)
+#         # print("device of mat: ", mat[-1].device)
+#     mat = np.array([tensor.cpu().numpy() for tensor in mat])
+
+#     return mat
         # np.save("patches/pcd_"+str(main_ind)+"/pnt_"+str(i)+".npy", mat)
         #     break
         # break
